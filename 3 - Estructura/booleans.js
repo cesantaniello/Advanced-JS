afbfbb586e2d22afa9c971295fa600fc66783a9c
Carlos <carlos.e.santaniello@gmail.com>
2020-09-20 19:33:56 +0200

    3.8-Expresiones-booleanas

---
+++ 3 - Estructura/booleans.js
@@ -0,0 +1,12 @@
+var estado = true,
+    resultado = "";
+/*
+if (estado){
+    console.log("continuar");
+} else {
+    console.log("detenerse");
+}
+*/
+
+//Operador ternario
+(estado) ? console.log("seguir") : console.log("parar");
