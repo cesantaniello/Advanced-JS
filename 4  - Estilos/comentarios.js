32fd76869bcf203626c56a479029e4ee5b4eab20
Carlos <carlos.e.santaniello@gmail.com>
2020-09-20 21:30:33 +0200

    4.2-abreviaturas y comentarios

---
+++ 4  - Estilos/comentarios.js
@@ -0,0 +1,9 @@
+/**
+ * Función para enviar saludos
+ * @param {string} nombre - Nombre de la persona a saludar 
+ */
+
+function saludar(nombre){
+    //Se retorna el mensaje formado
+    return "Hola "+ nombre;
+}
