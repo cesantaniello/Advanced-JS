450d932f63a341645505522ff1c5af9c662a10c3
Carlos <carlos.e.santaniello@gmail.com>
2020-09-20 22:54:44 +0200

    6.2-join vs concat

---
+++ 6 - Performance/join-vs-concat.js
@@ -0,0 +1,11 @@
+var animales = ["perro", "gato", "pez"];
+
+var listaAnimales = animales.join(", ");
+
+var nuevaLista = animales.concat(",");
+
+console.log(listaAnimales);
+console.log(typeof listaAnimales);  //Es un string
+
+console.log(nuevaLista);
+console.log(typeof nuevaLista); //Es un objeto
