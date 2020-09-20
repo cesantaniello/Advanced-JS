c4a99d01196f37929fec3c865a1840b69dd41339
Carlos <carlos.e.santaniello@gmail.com>
2020-09-20 22:23:30 +0200

    5.4-bloques

---
+++ 5 - Organizacion de Codigo/bloques.js
@@ -0,0 +1,12 @@
+var estado = true;
+
+/*
+if(estado){
+    console.log("Avanzar")
+}
+*/
+
+//Para bloques de una línea de código se pueden obviar las llaves
+
+if(estado)
+    console.log("Avanzar")
